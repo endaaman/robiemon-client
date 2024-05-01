105dbfcc83c0362e41e7959e11d1202daf874ed3
endaaman <ken@endaaman.me>
2024-05-01 17:29:15 +0900

    ported echart component

---
+++ src/lib/chart.svelte
@@ -0,0 +1,49 @@
+<!-- PORTED FROM: https://github.com/bherbruck/svelte-echarts rev:5ce2884d6671665eb96e5c7017dd0d548f67fa22-->
+<script>
+  import * as echarts from 'echarts'
+
+  const DEFAULT_OPTIONS = {
+    theme: undefined,
+    renderer: 'canvas',
+  }
+
+  export let options
+  export let { theme, renderer } = DEFAULT_OPTIONS
+
+  export function chartable(element, echartOptions) {
+    const { theme, renderer, options } = {
+      ...DEFAULT_OPTIONS,
+      ...echartOptions,
+    }
+    const echartsInstance = echarts.init(element, theme, { renderer })
+    echartsInstance.setOption(options, true)
+
+    function handleResize() {
+      echartsInstance.resize()
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return {
+      destroy() {
+        echartsInstance.dispose()
+        window.removeEventListener('resize', handleResize)
+      },
+      update(newOptions) {
+        echartsInstance.setOption({
+          ...echartOptions.options,
+          ...newOptions.options,
+        })
+      },
+    }
+  }
+</script>
+
+<div class="chart" use:chartable={{ renderer, theme, options }} />
+
+<style>
+  .chart {
+    height: 100%;
+    width: 100%;
+  }
+</style>
